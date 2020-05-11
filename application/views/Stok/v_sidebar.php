040e1deee7930f7adbf235e2f63c96fea42b0bd4
zorayaw <60455322+zorayaw@users.noreply.github.com>
2020-05-11 23:41:48 +0700

    Akses owner di Stock fixed

--- application/views/Stok/v_sidebar.php
+++ application/views/Stok/v_sidebar.php
@@ -12,10 +12,10 @@
         <li>
           <a href="<?php echo base_url()?>Stok/Stock"><i class="ti-calendar"></i><span class="right-nav-text">Stock</span> </a>
         </li>
-        <!-- <li>
+        <li>
           <a href="<?php echo base_url()?>Stok/Pemesanan/Kurir"><i class="ti-calendar"></i><span class="right-nav-text">Tagihan Ekspedisi</span> </a>
         </li>
-        <li>
+        <!-- <li>
           <a href="<?php echo base_url()?>"><i class="ti-calendar"></i><span class="right-nav-text">History Ekspedisi</span> </a>
         </li> -->
 
