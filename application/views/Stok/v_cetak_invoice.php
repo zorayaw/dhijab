045ae64261f097cce47fd6458f5e9d0555b3fd0c
cindy2400 <cdyy2400@gmail.com>
2020-05-14 13:00:40 +0700

    logo header

--- application/views/Stok/v_cetak_invoice.php
+++ application/views/Stok/v_cetak_invoice.php
@@ -11,7 +11,7 @@
 </head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <!-- Favicon -->
-<link rel="shortcut icon" href="<?php echo base_url()?>assets/images/logo.png" />
+<link rel="shortcut icon" href="<?php echo base_url()?>assets/admin/images/dhijablogo.jpeg" />
 
 <!-- Font -->
 <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
